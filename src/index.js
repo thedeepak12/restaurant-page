441406f62851cbec27e726b55565e5390f318297
Deepak <coding.deepak12@outlook.com>
2025-05-18 19:08:07 +0530

    Import tabs

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./style.css";
+import home from "./home.js";
+import menu from "./menu.js";
+import about from "./about.js";
+
+function setUpTabs() {
+    const buttons = document.querySelectorAll("header nav button");
+
+    buttons[0].addEventListener("click", home);
+    buttons[1].addEventListener("click", menu);
+    buttons[2].addEventListener("click", about);
+}
+
+home();
+menu();
+about();
+
+setUpTabs();
