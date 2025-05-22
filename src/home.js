982433d01f946476fa287c4dcfe60c4e34d45823
Deepak <coding.deepak12@outlook.com>
2025-05-22 10:46:35 +0530

    Add homepage background image

--- src/home.js
+++ src/home.js
@@ -1,3 +1,12 @@
+import restaurantImg from "./assets/images/restaurant.png";
+
 export default function home() {
-    
+    const content = document.querySelector("#content");
+    content.textContent = "";
+
+    const image = document.createElement("img");
+    image.src = restaurantImg;
+    image.classList.add("background-img");
+
+    content.appendChild(image);
 }
