38ad4afbdff78be91a05fc4cf0a17304a32ff901
Deepak <coding.deepak12@outlook.com>
2025-05-23 21:38:56 +0530

    Create basic structure for menu

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,38 @@
-export default function menu() {
+import restaurantImg from "./assets/images/restaurant.png";
 
+export default function menu() {
+    const content = document.querySelector("#content");
+
+    const menuContainer = document.createElement("div");
+    const image = document.createElement("img");
+    image.src = restaurantImg;
+    image.classList.add("background-img");
+    menuContainer.classList.add("menu-container");
+
+    const menuSections = [
+        {
+            title: "Main Dishes",
+            items: [
+                {
+
+                },
+            ],
+        },
+        {
+            title: "Sides",
+            items: [
+                {
+
+                },
+            ],
+        },
+        {
+            title: "Beverages",
+            items: [
+                {
+
+                },
+            ],
+        },
+    ];
 }
